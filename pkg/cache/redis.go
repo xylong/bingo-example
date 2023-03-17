3ce16736cf72966a96ad7101107d585cba20b33c
xyl <416319808@qq.com>
2023-03-17 17:39:00 +0800

    redis快捷方法

--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -1,22 +1,147 @@
 package cache
 
 import (
+	"bingo-example/pkg/logger"
 	"context"
 	"github.com/redis/go-redis/v9"
+	"sync"
+	"time"
 )
 
-var rdb *redis.Client
-
-// Redis 获取redis
-func Redis() *redis.Client {
-	return rdb
+type RedisClient struct {
+	*redis.Client
+	context.Context
 }
 
-// Connect 连接redis
-func Connect(options *redis.Options) {
-	rdb = redis.NewClient(options)
+func NewRedisClient(options *redis.Options) *RedisClient {
+	rds := &RedisClient{}
+	rds.Context = context.Background()
 
-	if _, err := rdb.Ping(context.Background()).Result(); err != nil {
-		panic(err)
+	rds.Client = redis.NewClient(options)
+	logger.IfInfo(rds.Ping())
+
+	return rds
+}
+
+// Ping 用以测试 redis 连接是否正常
+func (rds *RedisClient) Ping() error {
+	_, err := rds.Client.Ping(rds.Context).Result()
+	return err
+}
+
+// Set 存储 key 对应的 value，且设置 expiration 过期时间
+func (rds *RedisClient) Set(key string, value interface{}, expiration time.Duration) bool {
+	if err := rds.Client.Set(rds.Context, key, value, expiration).Err(); err != nil {
+		logger.ErrorString("Redis", "Set", err.Error())
+		return false
 	}
+	return true
+}
+
+// Get 获取 key 对应的 value
+func (rds *RedisClient) Get(key string) string {
+	result, err := rds.Client.Get(rds.Context, key).Result()
+	if err != nil {
+
+		if err != redis.Nil {
+			logger.ErrorString("Redis", "Get", err.Error())
+		}
+
+		return ""
+	}
+	return result
+}
+
+// Has 判断一个 key 是否存在，内部错误和 redis.Nil 都返回 false
+func (rds *RedisClient) Has(key string) bool {
+	_, err := rds.Client.Get(rds.Context, key).Result()
+	if err != nil {
+
+		if err != redis.Nil {
+			logger.ErrorString("Redis", "Has", err.Error())
+		}
+
+		return false
+	}
+	return true
+}
+
+// Del 删除存储在 redis 里的数据，支持多个 key 传参
+func (rds *RedisClient) Del(keys ...string) bool {
+	if err := rds.Client.Del(rds.Context, keys...).Err(); err != nil {
+		logger.ErrorString("Redis", "Del", err.Error())
+		return false
+	}
+
+	return true
+}
+
+// FlushDB 清空当前 redis db 里的所有数据
+func (rds *RedisClient) FlushDB() bool {
+	if err := rds.Client.FlushDB(rds.Context).Err(); err != nil {
+		logger.ErrorString("Redis", "FlushDB", err.Error())
+		return false
+	}
+
+	return true
+}
+
+// Increment 当参数只有 1 个时，为 key，其值增加 1。
+// 当参数有 2 个时，第一个参数为 key ，第二个参数为要增加的值 int64 类型。
+func (rds *RedisClient) Increment(parameters ...interface{}) bool {
+	switch len(parameters) {
+	case 1:
+		key := parameters[0].(string)
+		if err := rds.Client.Incr(rds.Context, key).Err(); err != nil {
+			logger.ErrorString("Redis", "Increment", err.Error())
+			return false
+		}
+	case 2:
+		key := parameters[0].(string)
+		value := parameters[1].(int64)
+		if err := rds.Client.IncrBy(rds.Context, key, value).Err(); err != nil {
+			logger.ErrorString("Redis", "Increment", err.Error())
+			return false
+		}
+	default:
+		logger.ErrorString("Redis", "Increment", "参数过多")
+		return false
+	}
+
+	return true
+}
+
+// Decrement 当参数只有 1 个时，为 key，其值减去 1。
+// 当参数有 2 个时，第一个参数为 key ，第二个参数为要减去的值 int64 类型。
+func (rds RedisClient) Decrement(parameters ...interface{}) bool {
+	switch len(parameters) {
+	case 1:
+		key := parameters[0].(string)
+		if err := rds.Client.Decr(rds.Context, key).Err(); err != nil {
+			logger.ErrorString("Redis", "Decrement", err.Error())
+			return false
+		}
+	case 2:
+		key := parameters[0].(string)
+		value := parameters[1].(int64)
+		if err := rds.Client.DecrBy(rds.Context, key, value).Err(); err != nil {
+			logger.ErrorString("Redis", "Decrement", err.Error())
+			return false
+		}
+	default:
+		logger.ErrorString("Redis", "Decrement", "参数过多")
+		return false
+	}
+	return true
+}
+
+var (
+	once  sync.Once
+	Cache *RedisClient
+)
+
+func Connect(options *redis.Options) {
+	once.Do(func() {
+		Cache = NewRedisClient(options)
+	})
 }
